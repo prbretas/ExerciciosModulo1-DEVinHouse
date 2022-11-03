10d6e3c2732dd49652cf8726a1b0e208ae842473
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2022-11-03 14:32:47 -0300

    Add files via upload

---
+++ PROVA - M1P1 Software House Ltda/main.js
@@ -0,0 +1,30 @@
+console.log("PROJETO 1 - DEVINHOUSE - EDP");
+console.log("Aluno: Philippe Rocca Bretas");
+
+//------------------------FORMULARIO------------------------------------------------------
+
+const save_tip_btn = document.getElementById("save-btn");
+const clear_form_btn = document.getElementById("clear-form-btn");
+
+//FUNÇÕES - FORMULÁRIO
+const createQuickTip = () => {};
+const clearForm = () => {};
+
+//-------------------------SEARCH TIPS-----------------------------------------------------
+
+const search_btn = document.getElementById("search-btn");
+const clear_search_btn = document.getElementById("clear-search-btn");
+
+// FUNÇÃO PROCURAR DICA
+const SearchTip = () => {};
+
+//-------------------------CARDS TIPS-----------------------------------------------------
+
+const card_delete = document.getElementById("card-delete");
+const card_edit = document.getElementById("card-edit");
+const card_video = document.getElementById("card-video");
+
+//FUNÇÃO CARDS QUICK TIPS
+const DeleteTip = () => {};
+const EditTip = () => {};
+const VideoTip = () => {};
